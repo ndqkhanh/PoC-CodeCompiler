c5056907af8ec117041d611cdfba61f1a1619b51
ndqkhanh20 <khanhndq2002@gmail.com>
2023-12-11 16:25:48 +0700

    fix something

--- routes/submission.route.js
+++ routes/submission.route.js
@@ -20,34 +20,33 @@ router.post("/", async function (req, res) {
       ? "codes/cplusplus.cpp"
       : "codes/python.py";
 
-  const source = readFileSync(new URL(`../${filepath}`, import.meta.url));
-
-  console.log(`Language ID: ${req.language_id}`);
-  console.log(`Source: ${source}`);
+  const source = readFileSync(
+    new URL(`../${filepath}`, import.meta.url)
+  ).toString();
 
   const options = {
-    method: 'POST',
+    method: "POST",
     url: `${process.env.COMPILER_URL}/submissions`,
     params: {
-      base64_encoded: 'false',
-      fields: "*"
+      base64_encoded: "false",
+      fields: "*",
+      wait: "true",
     },
     headers: {
-      'content-type': 'application/json',
-      'Content-Type': 'application/json',
-      'X-RapidAPI-Key': process.env.RAPID_APIKEY,
-      'X-RapidAPI-Host': process.env.HOST,
+      "content-type": "application/json",
+      "Content-Type": "application/json",
+      "X-RapidAPI-Key": process.env.RAPID_APIKEY,
+      "X-RapidAPI-Host": process.env.RAPID_APIHOST,
     },
     data: {
       language_id: req.body.language_id,
       source_code: source,
-      stdin: '1\n2'
-    }
-  }
+      stdin: "1\n2",
+    },
+  };
   try {
     const response = await axios.request(options);
-    console.log(JSON.stringify(response.data))
-    
+    console.log(JSON.stringify(response.data));
   } catch (error) {
     console.error(error);
   }
