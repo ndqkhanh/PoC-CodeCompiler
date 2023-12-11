d844e1b66542d045526ebb975d5e0ff266765758
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2023-12-11 17:59:45 +0700

    batch submissions

--- routes/submission.route.js
+++ routes/submission.route.js
@@ -13,10 +13,11 @@ const outputs = [43, 27, 17];
 const router = express.Router();
 
 router.post("/", async function (req, res) {
+  const {language_id} = req.body;
   const filepath =
-    req.body.language_id === 91
+  language_id === 91
       ? "codes/Main.java"
-      : req.body.language_id === 54
+      : language_id === 54
       ? "codes/cplusplus.cpp"
       : "codes/python.py";
 
@@ -26,11 +27,9 @@ router.post("/", async function (req, res) {
 
   const options = {
     method: "POST",
-    url: `${process.env.COMPILER_URL}/submissions`,
+    url: `${process.env.COMPILER_URL}/submissions/batch`,
     params: {
-      base64_encoded: "false",
-      fields: "*",
-      wait: "true",
+      base64_encoded: "false"
     },
     headers: {
       "content-type": "application/json",
@@ -39,19 +38,57 @@ router.post("/", async function (req, res) {
       "X-RapidAPI-Host": process.env.RAPID_APIHOST,
     },
     data: {
-      language_id: req.body.language_id,
-      source_code: source,
-      stdin: "1\n2",
-    },
+      submissions: [
+        {
+          language_id: language_id,
+          source_code: source,
+          stdin: `${inputs[0][0]}\n${inputs[0][1]}`,
+          cpu_time_limit: 1,
+        },
+        {
+          language_id: language_id,
+          source_code: source,
+          stdin: `${inputs[1][0]}\n${inputs[1][1]}`,
+          cpu_time_limit: 1,
+        },
+        {
+          language_id: language_id,
+          source_code: source,
+          stdin: `${inputs[2][0]}\n${inputs[2][1]}`,
+          cpu_time_limit: 1,
+        }
+      ]
+    }
   };
   try {
     const response = await axios.request(options);
-    console.log(JSON.stringify(response.data));
+
+    const options2 = {
+      method: 'GET',
+      url: `${process.env.COMPILER_URL}/submissions/batch`,
+      params: {
+        tokens: `${response.data[0].token},${response.data[1].token},${response.data[2].token}`,
+        base64_encoded: 'false',
+        fields: 'stdout,time,memory,stderr,compile_output,message'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.RAPID_APIKEY,
+        'X-RapidAPI-Host': process.env.RAPID_APIHOST
+      }
+    };
+    setTimeout(async()=>{
+      const response2 = await axios.request(options2);
+      const data = response2.data.submissions;
+      data.forEach ((d, i)=>{
+        d.pass = (d.stdout == outputs[i])
+      })
+      res.json(data)
+    }, 4000)
   } catch (error) {
     console.error(error);
   }
 
-  res.status(201).json([]);
+  
 });
 
 export default router;
