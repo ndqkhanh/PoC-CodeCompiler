51f58eace8d3dc284fb4298b5264ffa97f278ba3
ndqkhanh20 <khanhndq2002@gmail.com>
2023-12-11 10:19:05 +0700

    build: read file from codes

---
+++ codes/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Result {
+    public static int sumOfTwoIntegers(int a, int b) {
+        return a + b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.sumOfTwoIntegers(a, b);
+
+        System.out.print(result);
+
+        bufferedReader.close();
+    }
+}
--- routes/submission.route.js
+++ routes/submission.route.js
@@ -1,8 +1,31 @@
 import express from "express";
+import { readFile } from "fs/promises";
+
+const input = [
+  [1, 42],
+  [4, 23],
+  [7, 10],
+];
+
+const output = [43, 27, 17];
 
 const router = express.Router();
 
 router.post("/", async function (req, res) {
+  const filepath =
+    req.language_id === 91
+      ? "codes/java.java"
+      : req.language_id === 54
+      ? "codes/cplusplus.py"
+      : "codes/python.py";
+
+  const source = JSON.parse(
+    await readFile(new URL(`../${filepath}`, import.meta.url))
+  );
+
+  console.log(`Language ID: ${req.language_id}`);
+  console.log(`Source: ${source}`);
+
   res.status(201).json([]);
 });
 
